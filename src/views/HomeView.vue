e7a40fff6d35e4264105d7e576f9f3ddc654d2ad
KunalGupta <g.kunal9@gmail.com>
2024-04-04 08:44:03 +0530

    vue poc delete complete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,41 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <div v-if="tasks.length">
+      <div v-for="task in tasks" :key="task.id">
+        <SingleTaskComponent :task="task" @delete="handleDelete" @complete="handleCompllete"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import SingleTaskComponent from "../components/SingleTaskComponent.vue";
 export default {
   name: "HomeView",
-  components: {},
+  components: { SingleTaskComponent },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/tasks/")
+      .then((res) => res.json())
+      .then((data) => (this.tasks = data))
+      .catch((err) => console.log(err.message));
+  },
+  methods: {
+    handleDelete(id) {
+      this.tasks = this.tasks.filter((tasks) => {
+        return tasks.id != id;
+      });
+    },
+    handleCompllete(id){
+      let p = this.tasks.find(task=>{
+        return task.id === id
+      })
+      p.complete = !p.complete
+    }
+  },
 };
 </script>
