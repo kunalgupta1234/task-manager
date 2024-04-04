e8ff8c1ebcfb510e40bba3d612de121dbf7786d2
KunalGupta <g.kunal9@gmail.com>
2024-04-04 12:53:55 +0530

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
+  <NavbarComponent />
   <router-view />
 </template>
 
+<script>
+import NavbarComponent from "./components/NavbarComponent.vue";
+
+export default {
+  components: { NavbarComponent },
+};
+</script>
+
 <style>
-body{
+body {
   background: #f2f2f2;
 }
 #app {
